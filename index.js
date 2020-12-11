5b6541a857d5d847f1c03829b860f0830037512b
Brian May <brian@linuxpenguins.xyz>
2020-12-12 10:47:20 +1100

    Various updates

--- index.js
+++ index.js
@@ -2,13 +2,22 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  console.log(`Hello world!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("build-args", `BUILD_DATE=${time}`);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
+
+  const time = (new Date()).toISOString();
+
+  const prefix = "refs/tags/v";
+  let version = "beta";
+  if (payload.ref.indexOf(PREFIX) == 0) {
+    // PREFIX is exactly at the beginning
+    version = payload.ref.slice(PREFIX.length);
+  }
+
+  const vcs_ref = payload.head_commit.id.substr(0,8);
+
+  core.setOutput("build-args", `BUILD_DATE=${time} VERSION=${version} VCS_REF=${vcs_ref}`);
 } catch (error) {
   core.setFailed(error.message);
 }
