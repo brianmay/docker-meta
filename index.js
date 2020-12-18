c2dde988503e85dfcf8ebd3e9cb633579b9e1a49
Brian May <brian@linuxpenguins.xyz>
2020-12-18 11:28:47 +1100

    Rename payload to context

--- index.js
+++ index.js
@@ -2,28 +2,28 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = github.context;
+  // Get the JSON webhook context for the event that triggered the workflow
+  const context = github.context;
   const time = (new Date()).toISOString();
 
   let version = "beta";
 
-  if (payload.ref) {
+  if (context.ref) {
     const version_prefix = "refs/tags/v";
-    if (payload.ref.indexOf(version_prefix) == 0) {
-      version = payload.ref.slice(version_prefix.length);
+    if (context.ref.indexOf(version_prefix) == 0) {
+      version = context.ref.slice(version_prefix.length);
     }
 
     const branch_prefix = "refs/heads/";
-    if (payload.ref.indexOf(branch_prefix) == 0) {
-      version = payload.ref.slice(branch_prefix.length);
+    if (context.ref.indexOf(branch_prefix) == 0) {
+      version = context.ref.slice(branch_prefix.length);
     }
-  } else if (payload.head_ref) {
+  } else if (context.head_ref) {
       // pull requests
-      version = payload.head_ref;
+      version = context.head_ref;
   }
 
-  const vcs_ref = payload.sha.substr(0,8);
+  const vcs_ref = context.sha.substr(0,8);
 
   core.startGroup(`Context info`);
   core.info(`BUILD_DATE: ${time}`);
